a2d3d69046cc60b0ebb8405937057ccdadbb3ec6
liamrl <liamrenaudlevine@gmail.com>
2024-10-20 18:20:35 -0400

    10/20/24

---
+++ index.js
@@ -0,0 +1,10 @@
+let count = 0;
+function increment(){
+    count = count + 1
+    document.getElementById("count-el").innerText = count
+}
+function save(){
+    let save1 = 0;
+    save1 = count
+    document.getElementById("save2").innerText = save2.innerText + save1 + "-"
+}
